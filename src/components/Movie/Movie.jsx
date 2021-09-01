dcd9512e2dd2a6161eeb9792c903698a5fdffeb7
Rafa Villarraso <rafavillarrasoboluda@iMac-de-Rafa.local>
2021-09-01 19:38:41 +0200

    se añade el codigo para mostrar el overview al pasar por encima de las pastillas de cada película

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './Movie.scss';
 import '../../../src/colors.scss'
 
-const Movie = ({movie}) => {
+const Movie = ({movie, overview}) => {
     return <div className='movie'>
         <h3>{movie.title}</h3>
         <img src={'http://image.tmdb.org/t/p/w300'+movie.poster_path} alt=''/>
-        <p>Valoración: <bold>{movie.vote_average}</bold></p>
+        <p><small>Valoración: </small><bold>{movie.vote_average}</bold></p>
+        <div className='movieover'> 
+        <p><h4>Overview</h4></p>
+        <p>{movie.overview}</p>
+        </div>
+        {/* <p><button> <a href="https://api.themoviedb.org/3/movie/{movie_id}/reviews?d8d9fc93da62143ba1f2babedea9cc4e&language=en-US&page=1">Ver review</a></button></p> */}
     </div>
 }
 
